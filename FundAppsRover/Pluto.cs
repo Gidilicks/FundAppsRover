0381928c45c576e9792b1dfd32f9839fa9706b56
adegokegiwa@yahoo.ca <adegokegiwa@yahoo.ca>
2018-09-07 11:08:52 +0100

    Testing

---
+++ FundAppsRover/Pluto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FundAppsRover
+{
+    public class Pluto : ILandingSurface
+    {
+        public Pluto(int xMaxCoordinate, int yMaxCoordinate)
+        {
+            this.Width = xMaxCoordinate;
+            this.Height = yMaxCoordinate;
+        }
+
+        public Pluto(string plutoCoordinates)
+        {
+            if (string.IsNullOrEmpty(plutoCoordinates))
+                throw new ArgumentException();
+
+            this.SplitMessageCoordinates(plutoCoordinates);
+        }
+
+        public virtual int Width { get; private set; }
+        public virtual int Height { get; private set; }
+
+        private static readonly char MessageSeperator = ' ';
+
+        private void SplitMessageCoordinates(string plutoCoordinates)
+        {
+            string[] coordinateMessage = plutoCoordinates.Split(Pluto.MessageSeperator);
+
+            int noOfMsgValues = 2;
+            int xCoordinateIdx = 0;
+            int yCoordinateIdx = 1;
+
+            if (coordinateMessage.Length != noOfMsgValues)
+                throw new ArgumentOutOfRangeException();
+
+            this.Width = Convert.ToInt32(coordinateMessage[xCoordinateIdx]);
+            this.Height = Convert.ToInt32(coordinateMessage[yCoordinateIdx]);
+        }
+    }
+}
---
+++ FundAppsRoverTest/RoverTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FundAppsRover;
+
+namespace FundAppsRoverTest
+{
+    [TestClass]
+    public class RoverTest
+    {
+        [TestMethod]
+        public void DeployRover()
+        {
+            int noMovements = 1;
+            int roverMoveOne = 0;
+
+            ILandingSurface landingSurface = new Pluto("10 10");
+            Rovers rovers = new Rovers(landingSurface);
+
+            //first movement from 0, 0 facing North
+            rovers.DeployRover("0 0 N", "F");
+            IRover moveOne = rovers[roverMoveOne];
+
+            Assert.IsTrue(rovers.Count == noMovements);
+
+            Assert.IsNotNull(moveOne);
+        }
+    }
+}
