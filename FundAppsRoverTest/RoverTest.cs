f35b1adbe129bf021f206f8ce93fa92bf43fbfe7
adegokegiwa@yahoo.ca <adegokegiwa@yahoo.ca>
2018-09-07 11:54:21 +0100

    Test coordinates of two moves

--- FundAppsRoverTest/RoverTest.cs
+++ FundAppsRoverTest/RoverTest.cs
@@ -7,15 +7,16 @@ namespace FundAppsRoverTest
     [TestClass]
     public class RoverTest
     {
+        int noMovements = 3;
+        int roverMoveOne = 0;
+        int roverMoveTwo = 1;
+        int roverMoveThree = 2;
+
+        ILandingSurface landingSurface = new Pluto("100 100");
+
         [TestMethod]
         public void RoverMovementTest()
         {
-            int noMovements = 3;
-            int roverMoveOne = 0;
-            int roverMoveTwo = 1;
-            int roverMoveThree = 2;
-
-            ILandingSurface landingSurface = new Pluto("100 100");
             Rovers rovers = new Rovers(landingSurface);
 
             //first movement from 0, 0 facing North
@@ -36,6 +37,26 @@ namespace FundAppsRoverTest
             Assert.IsNotNull(moveOne);
             Assert.IsNotNull(moveTwo);
             Assert.IsNotNull(moveThree);
+        }
+
+        [TestMethod]
+        public void RoverMovementAndCoordinates()
+        {
+            Rovers rovers = new Rovers(landingSurface);
+
+            //first movement from 0, 0 facing North
+            rovers.DeployRover("0 0 N", "F");
+            IRover roverOne = rovers[roverMoveOne];
+
+            //second movement
+            rovers.DeployRover((roverOne.XCoordinate.ToString() + " " + roverOne.YCoordinate.ToString() + " " + roverOne.DirectionFacing), "FLBR");
+            IRover roverTwo = rovers[roverMoveTwo];
+
+            //assert coordinate of first movement from 0 0 North
+            Assert.IsTrue((roverOne.XCoordinate.ToString() + " " + roverOne.YCoordinate.ToString() + " " + roverOne.DirectionFacing) == "0 1 N", "Move 1 Coordinate != 0 1 N");
+            
+            //assert coordinate of second movement from previous coordinate
+            Assert.IsTrue((roverTwo.XCoordinate.ToString() + " " + roverTwo.YCoordinate.ToString() + " " + roverTwo.DirectionFacing) == "1 2 N", "Move 2 Coordinate != 1 2 N");
         }
     }
 }
