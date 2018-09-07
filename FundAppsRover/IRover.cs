33959b53f57d7b5dc31564553862c0a399561ad7
adegokegiwa@yahoo.ca <adegokegiwa@yahoo.ca>
2018-09-07 10:44:02 +0100

    Create rover and landing surface interfaces.

---
+++ FundAppsRover/IRover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FundAppsRover
+{
+    public interface IRover
+    {
+        int XCoordinate { get; set; }
+        int YCoordinate { get; set; }
+        string DirectionFacing { get; set; }
+        List<IRover> Moves { get; set; }
+    }
+}
---
+++ FundAppsRover/Rovers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FundAppsRover
+{
+    public class Rovers : List<IRover>
+    {
+        public Rovers(ILandingSurface landingSurface) 
+        {
+            this.LandingSurface = landingSurface;
+        }
+
+        public ILandingSurface LandingSurface { get; private set; }
+
+        public void DeployRover(string roverPosition, string roverCommands)
+        {
+            this.Add(new Rover(this, this.LandingSurface, roverPosition, roverCommands));
+        }
+    }
+}
