8fe586ba30ecf7344b553b6c61b7c0a825d58762
adegokegiwa@yahoo.ca <adegokegiwa@yahoo.ca>
2018-09-07 11:22:00 +0100

    testing Rover movements

--- FundAppsRoverTest/RoverTest.cs
+++ FundAppsRoverTest/RoverTest.cs
@@ -8,21 +8,34 @@ namespace FundAppsRoverTest
     public class RoverTest
     {
         [TestMethod]
-        public void DeployRover()
+        public void RoverMovementTest()
         {
-            int noMovements = 1;
+            int noMovements = 3;
             int roverMoveOne = 0;
+            int roverMoveTwo = 1;
+            int roverMoveThree = 2;
 
-            ILandingSurface landingSurface = new Pluto("10 10");
+            ILandingSurface landingSurface = new Pluto("100 100");
             Rovers rovers = new Rovers(landingSurface);
 
             //first movement from 0, 0 facing North
             rovers.DeployRover("0 0 N", "F");
             IRover moveOne = rovers[roverMoveOne];
 
+            //second movement
+            rovers.DeployRover(moveOne.XCoordinate + " " + moveOne.YCoordinate + " " + moveOne.DirectionFacing, "F");
+            IRover moveTwo = rovers[roverMoveTwo];
+
+            //third movement
+            rovers.DeployRover(moveTwo.XCoordinate + " " + moveTwo.YCoordinate + " " + moveTwo.DirectionFacing, "F");
+            IRover moveThree = rovers[roverMoveThree];
+
+            //number of movements
             Assert.IsTrue(rovers.Count == noMovements);
 
             Assert.IsNotNull(moveOne);
+            Assert.IsNotNull(moveTwo);
+            Assert.IsNotNull(moveThree);
         }
     }
 }
